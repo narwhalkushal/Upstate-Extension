5bfcbb89ad9847d2d4d569512eeaf0134e655920
Zach Visco <zvisco@gmail.com>
2018-06-22 19:30:11 -0400

    kinda works

--- js/modifyCSSValues.js
+++ js/modifyCSSValues.js
@@ -1,12 +1,21 @@
-$(document).ready(function modifyCSSvalues(){
+$(document).ready(function modifyCSSvalues() {
 
+  // function to set right column to same height as left column
   var elementSpacing = getComputedStyle(document.body);
   elementSpacing = elementSpacing.getPropertyValue('--elementspacing');
-  elementSpacing = parseInt(elementSpacing.replace(/px/,""));
+  elementSpacing = parseInt(elementSpacing.replace(/px/, ""));
   var divHeight = $('.left-column').outerHeight();
 
   var finalHeight = divHeight - elementSpacing;
   $('.weather-pane').outerHeight(finalHeight);
-  console.log(divHeight);
-  console.log(elementSpacing);
-  });
+
+  var buttonSpacing = $('.left-column').outerWidth();
+  var buttonWidth = (buttonSpacing - 2*elementSpacing)/3;
+  console.log(buttonWidth)
+  console.log(buttonSpacing)
+  $('.btn-icon').outerWidth(buttonWidth);
+  console.log($('.right-column').outerWidth())
+  console.log($('.btn-icon').outerWidth())
+
+
+});
