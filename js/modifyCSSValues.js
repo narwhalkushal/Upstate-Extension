a51c74276c3f5728698a4be4b4fbceed16bb1e89
Zach Visco <zvisco@gmail.com>
2018-06-23 02:02:12 -0400

    :'(

--- js/modifyCSSValues.js
+++ js/modifyCSSValues.js
@@ -1,23 +1,33 @@
 $(document).ready(function modifyCSSvalues() {
 
   // function to set right column to same height as left column
-  var elementSpacing = getComputedStyle(document.body);
-  elementSpacing = elementSpacing.getPropertyValue('--elementspacing');
+  var docValues = getComputedStyle(document.body);
+  elementSpacing = docValues.getPropertyValue('--elementspacing');
   elementSpacing = parseInt(elementSpacing.replace(/px/, ""));
+
+  docWidth = docValues.getPropertyValue('--maxwidth');
+  docWidth = parseInt(docWidth.replace(/px/, ""));
+
   var divHeight = $('.link-btn').outerHeight();
 
-  $('.left-column').outerHeight((5*divHeight + 4*elementSpacing))
+  $('.left-column').outerWidth(.5*(docWidth-elementSpacing));
+  $('.right-column').outerWidth(.5*(docWidth-elementSpacing));
+  $('.left-column').outerHeight((5*divHeight + 4*elementSpacing));
 
   var leftHeight = $('.left-column').outerHeight();
   // var finalHeight = divHeight - elementSpacing;
   $('.weather-pane').outerHeight(leftHeight);
 
-  var buttonSpacing = $('.header-pane').outerWidth();
-  var buttonWidth = (buttonSpacing-5*elementSpacing)/6;
+  var buttonSpacing = $('.left-column').outerWidth();
+  var buttonWidth = (buttonSpacing-2*elementSpacing)/3;
   $('.btn-icon').outerWidth(buttonWidth);
   $('.btn-icon').outerHeight(buttonWidth);
-  console.log($('.container').outerWidth())
+
+  console.log($('.header-pane').outerWidth());
+
+  console.log(elementSpacing)
   console.log($('.left-column').outerWidth())
+  console.log($('.right-column').outerWidth())
 
   // var left = $(".link-btn").offset().left;
   // $('.btn-icon').css('right', elementSpacing);
