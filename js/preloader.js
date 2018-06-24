2b4e028b7b3121faa12bce598aafd545be7d01a9
Zach Visco <zvisco@gmail.com>
2018-06-24 14:39:53 -0400

    All finished and uploaded

--- js/preloader.js
+++ js/preloader.js
@@ -1,6 +1,12 @@
 // makes sure the whole site is loaded
 $(window).ready( function() {
   // will first fade out the loading animation
+
+  if ($(window).outerWidth() < 680){
+    $('#preloader').css('font-size', '25px');
+  } else {
+    $('#preloader').css('font-size', '35px');
+  }
   $("#status").delay(600).fadeOut('slow');
 
   // will fade out the whole DIV that covers the website.
