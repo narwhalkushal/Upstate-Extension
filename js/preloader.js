4cf8937a1b99dc5ca35cc6331bc5fec3c388690d
Zach Visco <zvisco@gmail.com>
2018-06-24 01:00:39 -0400

    Adding readme

--- js/preloader.js
+++ js/preloader.js
@@ -1,9 +1,8 @@
 // makes sure the whole site is loaded
 $(window).ready( function() {
   // will first fade out the loading animation
-  // $('#status').fadeIn('fast');
-  $("#status").delay(1000).fadeOut('slow');
+  $("#status").delay(400).fadeOut('slow');
 
   // will fade out the whole DIV that covers the website.
-  $("#preloader").delay(1000).fadeOut("slow");
+  $("#preloader").delay(800).fadeOut("slow");
 })
