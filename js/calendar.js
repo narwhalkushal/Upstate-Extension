50355c5395a82db531d34b6c47b45be3b6498d18
Zach Visco <zvisco@gmail.com>
2018-07-18 09:06:57 -0400

    Updating .gitignore

--- js/calendar.js
+++ js/calendar.js
@@ -1,17 +1,16 @@
 $(function() {
-  $('#calendar').fullCalendar({
-    googleCalendarApiKey: googleCalKey,
-    events: {
-      googleCalendarId: '0g5lutmp776hpisbovsd7thojo42o924@import.calendar.google.com'
-  },
-  defaultView: 'basicWeek',
-  header: {
-  left:   'title',
-  center: '',
-  right:  'today prev,next'
-},
-height: 300,
-
-  });
-  // $('#calendar').css('background-color', 'black');
+    $('#calendar').fullCalendar({
+        googleCalendarApiKey: googleCalKey,
+        events: {
+            googleCalendarId: '0g5lutmp776hpisbovsd7thojo42o924@import.calendar.google.com'
+        },
+        defaultView: 'basicWeek',
+        header: {
+            left: '',
+            center: 'title',
+            right: ''
+        },
+        height: 180,
+    });
+    // $('#calendar').css('background-color', 'black');
 });
