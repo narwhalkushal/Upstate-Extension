dbfd597221d8fdd34a3f7a7b88c12d1cbd21a4f3
Zach Visco <zvisco@gmail.com>
2018-07-18 00:30:54 -0400

    Calendar seems to be working

---
+++ js/calendar.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('#calendar').fullCalendar({
+    googleCalendarApiKey: 'AIzaSyCUhz4-B4d0GyJhw3o-tsuskGvIckhgd04',
+    events: {
+      googleCalendarId: '0g5lutmp776hpisbovsd7thojo42o924@import.calendar.google.com'
+    }
+  });
+});
