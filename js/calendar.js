3f0ddd470f9c597964fc3f513b86aa1f48a71462
Zach Visco <zvisco@gmail.com>
2018-07-18 01:05:33 -0400

    adjusting settings

--- js/calendar.js
+++ js/calendar.js
@@ -1,8 +1,17 @@
 $(function() {
   $('#calendar').fullCalendar({
-    googleCalendarApiKey: 'AIzaSyCUhz4-B4d0GyJhw3o-tsuskGvIckhgd04',
+    googleCalendarApiKey: googleCalKey,
     events: {
       googleCalendarId: '0g5lutmp776hpisbovsd7thojo42o924@import.calendar.google.com'
-    }
+  },
+  defaultView: 'basicWeek',
+  header: {
+  left:   'title',
+  center: '',
+  right:  'today prev,next'
+},
+height: 300,
+
   });
+  // $('#calendar').css('background-color', 'black');
 });
