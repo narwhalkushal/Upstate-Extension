dfb0f660f2cdec77ded2dbfa670af9a0f8095ee3
Zach Visco <zvisco@gmail.com>
2018-07-21 16:25:18 -0400

    Updating files

--- js/preloader.js
+++ js/preloader.js
@@ -1,11 +1,7 @@
 // makes sure the whole site is loaded
 $(window).ready( function() {
   // will first fade out the loading animation
-  console.log($(window).outerWidth());
-  //
-  $("#status").delay(200).fadeOut('slow');
-  //
+  $("#status").delay(300).fadeOut('slow');
   // // will fade out the whole DIV that covers the website.
-  $("#preloader").delay(300).fadeOut("slow");
-
+  $("#preloader").delay(400).fadeOut("fast");
 });
