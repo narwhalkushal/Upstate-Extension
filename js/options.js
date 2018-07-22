de837d77a46521b42d44bd084b808b456359e6c2
Zach Visco <zvisco@gmail.com>
2018-07-22 11:40:58 -0400

    Updates

--- js/options.js
+++ js/options.js
@@ -1,79 +1,31 @@
-// Credits to: https://davidsimpson.me/2014/05/27/add-googles-universal-analytics-tracking-chrome-extension/
-// Count number of useres
-// (function(i, s, o, g, r, a, m) {
-//     i['GoogleAnalyticsObject'] = r;
-//     i[r] = i[r] || function() {
-//
-//         (i[r].q = i[r].q || []).push(arguments)
-//     }, i[r].l = 1 * new Date();
-//     a = s.createElement(o),
-//
-//         m = s.getElementsByTagName(o)[0];
-//     a.async = 1;
-//     a.src = g;
-//     m.parentNode.insertBefore(a, m)
-//
-// })(window, document, 'script', 'https://www.google-analytics.com/analytics_debug.js', 'ga'); // Note: https protocol here
+(function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga'); // Note: https protocol here
 var GA_LOCAL_STORAGE_KEY = 'ga:clientId';
 
 if (window.localStorage) {
-  ga('create', uaGoogleId, {
-    'storage': 'none',
-    'clientId': localStorage.getItem(GA_LOCAL_STORAGE_KEY)
-  });
-  ga(function(tracker) {
-    localStorage.setItem(GA_LOCAL_STORAGE_KEY, tracker.get('clientId'));
-  });
-}
-else {
-  ga('create', uaGoogleId, 'auto');
+    ga('create', uaGoogleId, {
+        'storage': 'none',
+        'clientId': localStorage.getItem(GA_LOCAL_STORAGE_KEY)
+    });
+    ga(function(tracker) {
+        localStorage.setItem(GA_LOCAL_STORAGE_KEY, tracker.get('clientId'));
+    });
+} else {
+    ga('create', uaGoogleId, 'auto');
 }
 
 ga('set', 'checkProtocolTask', function() {}); // Removes failing protocol check. @see: http://stackoverflow.com/a/22152353/1958200
-
 ga('require', 'displayfeatures');
-
-ga('send', 'pageview', '/options.html');
-
-// /**
-//  * Google Analytics JS v1
-//  * http://code.google.com/p/google-analytics-js/
-//  * Copyright (c) 2009 Remy Sharp remysharp.com / MIT License
-//  * $Date: 2009-02-25 14:25:01 +0000 (Wed, 25 Feb 2009) $
-//  */
-// function gaTrack(urchinCode, domain, url) {
-//
-//     function rand(min, max) {
-//         return min + Math.floor(Math.random() * (max - min));
-//     }
-//
-//     var i = 1000000000,
-//         utmn = rand(i, 9999999999), //random request number
-//         cookie = rand(10000000, 99999999), //random cookie number
-//         random = rand(i, 2147483647), //number under 2147483647
-//         today = (new Date()).getTime(),
-//         win = window.location,
-//         img = new Image(),
-//         urchinUrl = 'http://www.google-analytics.com/__utm.gif?utmwv=1.3&utmn='
-//         +utmn+'&utmsr=-&utmsc=-&utmul=-&utmje=0&utmfl=-&utmdt=-&utmhn='
-//         +domain+'&utmr='+win+'&utmp='
-//         +url+'&utmac='
-//         +urchinCode+'&utmcc=__utma%3D'
-//         +cookie+'.'+random+'.'+today+'.'+today+'.'
-//         +today+'.2%3B%2B__utmb%3D'
-//         +cookie+'%3B%2B__utmc%3D'
-//         +cookie+'%3B%2B__utmz%3D'
-//         +cookie+'.'+today
-//         +'.2.2.utmccn%3D(referral)%7Cutmcsr%3D' + win.host + '%7Cutmcct%3D' + win.pathname + '%7Cutmcmd%3Dreferral%3B%2B__utmv%3D'
-//         +cookie+'.-%3B';
-//     console.log(urchinUrl);
-//
-//     // trigger the tracking
-//     img.src = urchinUrl;
-// }
-//
-// window.addEventListener("load", function() {
-//
-//     gaTrack(uaGoogleId, 'accessUpstate', 'test.html');
-//
-// }, false);
+ga('send', 'pageview', location.pathname);
